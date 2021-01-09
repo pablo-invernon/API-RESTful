692ea9df598118ed9309c6108bf7a5cc9bdf810b
Pablo Invernón Moreno <pablo_invernon@inetum.world>
2021-01-09 14:36:00 +0100

    test unitarios completados

--- scripts/docker-make.cmd
+++ scripts/docker-make.cmd
@@ -1,4 +1,3 @@
-@echo "INVOCACION INTERACTIVA"
 
 @call scripts\docker-scripts-common.cmd
 
@@ -6,5 +5,4 @@
 docker container run -ti --rm --env-file scripts\py.env ^
     -v %CD%\docker\credentials:/root/.aws/credentials ^
     -v %CD%\docker\config:/root/.aws/config ^
-    -v %CD%:/usr/src/todo-list-serverless %IMAGE% /bin/sh
-
+    -v %CD%:/usr/src/todo-list-serverless %IMAGE% make %1
